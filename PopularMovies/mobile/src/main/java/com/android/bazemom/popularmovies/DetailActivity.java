e6c1f5b905a6ee3f418803e535131f630def1048
bbkiszka@gmail.com <bbazemore@users.noreply.github.com>
2015-09-27 09:04:05 -0400

    Add ViewHolder to DetailFragment to speed up the UI.

--- PopularMovies/mobile/src/main/java/com/android/bazemom/popularmovies/DetailActivity.java
+++ PopularMovies/mobile/src/main/java/com/android/bazemom/popularmovies/DetailActivity.java
@@ -60,6 +60,7 @@ public class DetailActivity extends AppCompatActivity {
 
         private int mMovieId;
         private View mRootView;
+        private DetailViewHolder mViewHolder;
         private MovieDetail mMovieDetail;
 
         // private ShareActionProvider mShareActionProvider;  // V2?
@@ -72,7 +73,13 @@ public class DetailActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Get the layout adjusted to the new orientation / device
             mRootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            // Get the ids of the View elements so we don't have to fetch them over and over
+            mViewHolder = new DetailViewHolder();
+            // slide nerd was doing mRootView.setTag(mViewHolder) - I'm just keeping it in a member variable
+
             int movieId;
 
             Intent intent = getActivity().getIntent();
@@ -119,22 +126,16 @@ public class DetailActivity extends AppCompatActivity {
                 return;
 
             final Context context = mRootView.getContext();
-            TextView titleView = (TextView) mRootView.findViewById(R.id.detail_movie_title);
-            titleView.setText(mMovieDetail.title);
-            TextView overview = (TextView) mRootView.findViewById(R.id.detail_movie_overview);
-            overview.setText(mMovieDetail.overview);
+            mViewHolder.titleView.setText(mMovieDetail.title);
+            mViewHolder.overview.setText(mMovieDetail.overview);
+            mViewHolder.releaseDate.setText(context.getString(R.string.detail_release_date) + mMovieDetail.releaseDate);
 
-            TextView releaseDate = (TextView) mRootView.findViewById(R.id.detail_movie_release_date);
-            releaseDate.setText(context.getString(R.string.detail_release_date) + mMovieDetail.releaseDate);
             // A little convoluted here to support internationalization later. Stuff the runtime
             // in minutes into a formatted string.  The placement of the number may vary in other languages.
-            TextView runtime = (TextView) mRootView.findViewById(R.id.detail_movie_runtime);
             String runtimeText = String.format(context.getString(R.string.detail_runtime_format), mMovieDetail.runtime);
-            runtime.setText(runtimeText);
+            mViewHolder.runtime.setText(runtimeText);
 
-            TextView rating = (TextView) mRootView.findViewById(R.id.detail_movie_user_rating);
-            rating.setText(context.getString(R.string.detail_movie_user_rating) + Double.toString(mMovieDetail.voteAverage));
-
+            mViewHolder.rating.setText(context.getString(R.string.detail_movie_user_rating) + Double.toString(mMovieDetail.voteAverage));
 
             // To build an image URL, we need 3 pieces of data. The baseurl, size and filepath.
             // First get the size from the preferences, user can select high, medium or low
@@ -143,21 +144,19 @@ public class DetailActivity extends AppCompatActivity {
 
             if (mMovieDetail.posterPath != null && !mMovieDetail.posterPath.isEmpty()) {
                 String posterURL = context.getString(R.string.TMDB_image_base_url) + posterSize + mMovieDetail.posterPath;
-                ImageView posterView = (ImageView) mRootView.findViewById(R.id.detail_movie_poster);
 
                 // Here’s an example URL: http://image.tmdb.org/t/p/w500/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg
                 Picasso.with(context)
                         .load(posterURL)
                                 //.placeholder(R.mipmap.ic_launcher) too busy looking
                         .error(R.mipmap.ic_error_fallback)         // optional
-                        .into(posterView);
+                        .into(mViewHolder.posterView);
             }
 
             // Now set the background image for the whole frame in the Detail View
             // Stolen from http://stackoverflow.com/questions/29777354/how-do-i-set-background-image-with-picasso-in-code
             // Note the image quality values are different for posters and backdrops, so fix up equivalent high, medium, and low values here.
             if (mMovieDetail.backdropPath != null && !mMovieDetail.backdropPath.isEmpty()) {
-                final RelativeLayout detailLayout = (RelativeLayout) mRootView.findViewById(R.id.detail_movie_background);
                 int backgroundSizeId = R.string.settings_backddrop_quality_high;
                 if (posterSize.equals(getString(R.string.settings_poster_quality_medium))) {
                     backgroundSizeId = R.string.settings_backdrop_quality_medium;
@@ -175,7 +174,7 @@ public class DetailActivity extends AppCompatActivity {
                     public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
                         // use deprecated setBackgroundDrawable for API 11 compatibility.
                         // setBackground requires 16 which is a bit too new for now
-                        detailLayout.setBackgroundDrawable(new BitmapDrawable(context.getResources(), bitmap));
+                        mViewHolder.detailLayout.setBackgroundDrawable(new BitmapDrawable(context.getResources(), bitmap));
                     }
 
                     @Override
@@ -188,6 +187,28 @@ public class DetailActivity extends AppCompatActivity {
                         Log.d("TAG", "Prepare background image Load");
                     }
                 });
+            }
+        }
+
+        // Handy dandy little class to cache the View ids so we don't keep looking for them every
+        // time we refresh the UI.  We only need to fetch them after the inflate in onCreateView
+        class DetailViewHolder {
+            TextView titleView;
+            TextView overview;
+            TextView releaseDate;
+            TextView runtime;
+            TextView rating;
+            ImageView posterView;
+            RelativeLayout detailLayout;
+
+            DetailViewHolder() {
+                titleView = (TextView) mRootView.findViewById(R.id.detail_movie_title);
+                overview = (TextView) mRootView.findViewById(R.id.detail_movie_overview);
+                releaseDate = (TextView) mRootView.findViewById(R.id.detail_movie_release_date);
+                runtime = (TextView) mRootView.findViewById(R.id.detail_movie_runtime);
+                rating = (TextView) mRootView.findViewById(R.id.detail_movie_user_rating);
+                posterView = (ImageView) mRootView.findViewById(R.id.detail_movie_poster);
+                detailLayout = (RelativeLayout) mRootView.findViewById(R.id.detail_movie_background);
             }
         }
 
