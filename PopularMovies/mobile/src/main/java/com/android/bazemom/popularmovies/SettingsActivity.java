8534981caa78abc40af9b0ab2a495094acd98243
bbkiszka@gmail.com <bbazemore@users.noreply.github.com>
2015-12-30 15:32:54 -0500

    Per reviewer feedback, update Preferences to use PreferenceFragment rather than older PreferenceActivity.

--- PopularMovies/mobile/src/main/java/com/android/bazemom/popularmovies/SettingsActivity.java
+++ PopularMovies/mobile/src/main/java/com/android/bazemom/popularmovies/SettingsActivity.java
@@ -4,76 +4,102 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
-        * A {@link PreferenceActivity} that presents a set of application settings.
-        * <p>
-        * See <a href="http://developer.android.com/design/patterns/settings.html">
-        * Android Design: Settings</a> for design guidelines and the <a
-        * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
-        * API Guide</a> for more information on developing a Settings UI.
-        */
-public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener {
+ * A {@link PreferenceActivity} that presents a set of application settings.
+ * <p/>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
+ */
+public class SettingsActivity extends AppCompatActivity {
+    private final static String TAG = SettingsActivity.class.getSimpleName();
+
+    SettingsFragment mSettingsFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Todo: this old style addPreferences is only needed if we are supporting API 10 or earlier.
-        // Since this app claims 11 or better this should get updated to use PreferenceFragments
-        // http://developer.android.com/reference/android/preference/PreferenceActivity.html
-        addPreferencesFromResource(R.xml.pref_general);
 
-        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
-
-        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
-        // updated when the preference changes.
-       bindPreferenceSummaryToValue(findPreference(getString(R.string.settings_sort_key)));
-       bindPreferenceSummaryToValue(findPreference(getString(R.string.settings_image_quality_key)));
+        // Display the Settings fragment as the main content
+        mSettingsFragment = new SettingsFragment();
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, mSettingsFragment)
+                .commit();
     }
 
-    /**
-     * Attaches a listener so the summary is always updated with the preference value.
-     * Also fires the listener once, to initialize the summary (so it shows up before the value
-     * is changed.)
-     */
-    private void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(this);
+    // Using a static inner class fragment as suggested by:
+    // http://codetheory.in/saving-user-settings-with-android-preferences/
+    public static class SettingsFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Make sure default values are applied.  Consider moving this to
+            // a shared function that is used to retrieve the
+            // SharedPreferences wherever they are needed.
+            PreferenceManager.setDefaultValues(getActivity(), R.xml.pref_general, false);
+            addPreferencesFromResource(R.xml.pref_general);
+            //Log.d(TAG, "Preferences from pref_general added in SettingsFragment.onCreate.");
 
-        // Trigger the listener immediately with the preference's
-        // current value.
-        onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
-        String stringValue = value.toString();
-
-        if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list (since they have separate labels/values).
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(stringValue);
-
-            if (prefIndex >= 0) {
-                // Avoid tripping extra UI updates if nothing changed.
-                CharSequence oldValue = listPreference.getSummary();
-                CharSequence newValue = listPreference.getEntries()[prefIndex];
-                if (oldValue != null && !newValue.equals(oldValue)) {
-                    preference.setSummary(newValue);
-                }
-            }
-        } else {
-            // For other preferences, set the summary to the value's simple string representation.
-            preference.setSummary(stringValue);
+            // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+            // updated when the preference changes.
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.settings_sort_key)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.settings_image_quality_key)));
         }
-        // Refresh UI with new preferences??
-        return true;
-    }
 
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list (since they have separate labels/values).
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+
+                if (prefIndex >= 0) {
+                    // Avoid tripping extra UI updates if nothing changed.
+                    CharSequence oldValue = listPreference.getSummary();
+                    CharSequence newValue = listPreference.getEntries()[prefIndex];
+                    if (oldValue != null && !newValue.equals(oldValue)) {
+                        preference.setSummary(newValue);
+                    }
+                }
+            } else {
+                // For other preferences, set the summary to the value's simple string representation.
+                preference.setSummary(stringValue);
+            }
+            // Refresh UI with new preferences??
+            return true;
+        }
+
+        /**
+         * Attaches a listener so the summary is always updated with the preference value.
+         * Also fires the listener once, to initialize the summary (so it shows up before the value
+         * is changed.)
+         */
+        protected void bindPreferenceSummaryToValue(Preference preference) {
+            // Set the listener to watch for value changes.
+            if (null == preference) {
+                Log.d(TAG, "Preference is null. Can't bind preference.");
+                return;
+            }
+            preference.setOnPreferenceChangeListener(this);
+
+            // Trigger the listener immediately with the preference's
+            // current value.
+            onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+
+            //Log.d(TAG, "Preference set for: " + preference.getKey());
+        }
+    } // end SettingsFragment
 }
